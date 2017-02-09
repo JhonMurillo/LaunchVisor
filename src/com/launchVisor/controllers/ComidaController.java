f4bb9df58dbbcb309fc5ebbc3fa5e5d5632e0d41
Jhon Mario Murillo Cordoba <jhonmurillo2014@gmail.com>
2017-02-08 21:45:50 -0500

    Actualizacion pon.xml

--- src/com/launchVisor/controllers/ComidaController.java
+++ src/com/launchVisor/controllers/ComidaController.java
@@ -1,19 +1,15 @@
-package com.launchVisor.controllers;
-
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
-import javax.faces.event.ActionEvent;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import com.launchVisor.services.TaskService;
-
-@ManagedBean(name = "comidaController", eager = true)
-@Component
-@RequestScoped
-public class ComidaController {
-
-
-
-}
+//package com.launchVisor.controllers;
+//
+//import javax.faces.bean.ManagedBean;
+//import javax.faces.bean.RequestScoped;
+//import org.springframework.stereotype.Component;
+//
+//
+//@ManagedBean(name = "comidaController", eager = true)
+//@Component
+//@RequestScoped
+//public class ComidaController {
+//
+//
+//
+//}
--- src/com/launchVisor/controllers/TaskController.java
+++ src/com/launchVisor/controllers/TaskController.java
@@ -1,20 +1,20 @@
-package com.launchVisor.controllers;
-
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
-import org.springframework.stereotype.Component;
-
-
-@ManagedBean(name = "taskController", eager = true)
-@Component
-@RequestScoped
-public class TaskController {
+//package com.launchVisor.controllers;
 //
-//    @Autowired
-//    private TaskService taskService;
+//import javax.faces.bean.ManagedBean;
+//import javax.faces.bean.RequestScoped;
+//import org.springframework.stereotype.Component;
 //
-////    public void startTasks(ActionEvent event) throws InterruptedException {
-////        this.taskService.startIdleTasks();
-////    }
-
-}
+//
+//@ManagedBean(name = "taskController", eager = true)
+//@Component
+//@RequestScoped
+//public class TaskController {
+////
+////    @Autowired
+////    private TaskService taskService;
+////
+//////    public void startTasks(ActionEvent event) throws InterruptedException {
+//////        this.taskService.startIdleTasks();
+//////    }
+//
+//}
